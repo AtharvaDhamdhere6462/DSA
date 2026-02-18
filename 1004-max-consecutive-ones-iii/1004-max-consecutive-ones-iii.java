d91ebc83d02e609fbf3243e176d313dc58c99265
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-02-18 16:57:35 +0530

    Time: 3 ms (98.12%), Space: 51.7 MB (98.11%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,21 +1,73 @@
+// class Solution {
+//     public int longestOnes(int[] nums, int k) {
+//         int n=nums.length;
+//         int maxlen=0;
+
+//         for(int i=0;i<n;i++){
+//             int zeros=0;
+//             for(int j=i;j<n;j++){
+//                 if(nums[j]==0){
+//                     zeros++;
+//                 }
+//                 if(zeros<=k){
+//                     int len=j-i+1;
+//                     maxlen=Math.max(maxlen,len);
+//                 }else{
+//                     break;
+//                 }
+//             }
+//         }
+//         return maxlen;
+//     }
+// }
+
+// class Solution {
+//     public int longestOnes(int[] nums, int k) {
+//         int n=nums.length;
+//         int left=0;
+//         int right=0;
+//         int maxlen=0;
+//         int zero =0;
+
+//         while(right<n){
+//             if(nums[right]==0){
+//                 zero++;
+//             }
+//             while(zero>k){
+//                 if(nums[left]==0){
+//                     zero--;
+                    
+//                 }
+//                 left++;
+//             }
+//             int len =right-left+1;
+//             maxlen =Math.max(maxlen,len);
+                
+//             right++;
+//         }
+//         return maxlen;
+//     }
+// }
+
 class Solution {
     public int longestOnes(int[] nums, int k) {
         int n=nums.length;
+        int left=0;
         int maxlen=0;
+        int zero =0;
 
-        for(int i=0;i<n;i++){
-            int zeros=0;
-            for(int j=i;j<n;j++){
-                if(nums[j]==0){
-                    zeros++;
-                }
-                if(zeros<=k){
-                    int len=j-i+1;
-                    maxlen=Math.max(maxlen,len);
-                }else{
-                    break;
-                }
+        for(int right=0;right<n;right++){
+            if(nums[right]==0){
+                zero++;
             }
+            while(zero>k){
+                if(nums[left]==0){
+                    zero--;
+                }
+                left++;
+            }
+            int len = right-left+1;
+            maxlen=Math.max(len,maxlen);
         }
         return maxlen;
     }
