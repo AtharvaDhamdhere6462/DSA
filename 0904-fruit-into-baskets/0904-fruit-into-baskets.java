b1ad94e9114255199adbeab2eba1c8525567d4f2
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-02-18 18:22:36 +0530

    Time: 57 ms (23.08%), Space: 71 MB (15.32%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,57 @@
+// class Solution {
+//     public int totalFruit(int[] fruits) {
+        
+//         int n=fruits.length;
+//         int maxlen=0;
+//         for(int i=0;i<n;i++){
+//             Set<Integer> st = new HashSet<>();
+//             for(int j=i;j<n;j++){
+//                 st.add(fruits[j]);
+//                 if(st.size()>2){
+//                     break;
+//                 }
+//                 maxlen=Math.max(maxlen,j-i+1);
+//             }
+//         }  
+//         return maxlen; 
+//     }
+// }
+
+
+import java.util.*;
+
+class Solution {
+    // Function to find the maximum number of fruits we can collect with at most two fruit types
+    public int totalFruit(int[] fruits) {
+        // HashMap to track count of each fruit in current window
+        Map<Integer, Integer> basket = new HashMap<>();
+
+        // Initialize pointers and max result
+        int left = 0;
+        int maxFruits = 0;
+
+        // Traverse the fruits array using right pointer
+        for (int right = 0; right < fruits.length; right++) {
+            // Include current fruit in the map
+            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
+
+            // If more than 2 fruit types, shrink window from left
+            while (basket.size() > 2) {
+                basket.put(fruits[left], basket.get(fruits[left]) - 1);
+
+                if (basket.get(fruits[left]) == 0) {
+                    basket.remove(fruits[left]);
+                }
+
+                left++;
+            }
+
+            // Update maximum valid window length
+            maxFruits = Math.max(maxFruits, right - left + 1);
+        }
+
+        // Return the final result
+        return maxFruits;
+    }
+}
+
