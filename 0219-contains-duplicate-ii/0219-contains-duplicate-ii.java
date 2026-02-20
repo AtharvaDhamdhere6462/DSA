0c8af408f391d1f92417919a95fe57562f65310e
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-02-20 08:20:47 +0530

    Time: 25 ms (37.89%), Space: 76.3 MB (78.56%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,23 +1,48 @@
+// import java.util.*;
+
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+
+//             // If number already seen
+//             if (map.containsKey(nums[i])) {
+//                 int prevIndex = map.get(nums[i]);
+
+//                 // Check distance
+//                 if (i - prevIndex <= k) {
+//                     return true;
+//                 }
+//             }
+
+//             // Update latest index
+//             map.put(nums[i], i);
+//         }
+
+//         return false;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> window = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
 
-            // If number already seen
-            if (map.containsKey(nums[i])) {
-                int prevIndex = map.get(nums[i]);
-
-                // Check distance
-                if (i - prevIndex <= k) {
-                    return true;
-                }
+            // If already present in window → duplicate within k
+            if (window.contains(nums[i])) {
+                return true;
             }
 
-            // Update latest index
-            map.put(nums[i], i);
+            window.add(nums[i]);
+
+            // Maintain window size at most k
+            if (window.size() > k) {
+                window.remove(nums[i - k]);
+            }
         }
 
         return false;
