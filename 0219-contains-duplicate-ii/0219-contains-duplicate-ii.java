2085ab693183098a7d6aa7f42ed8bfd5685e49ad
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-02-20 08:18:35 +0530

    Time: 28 ms (16.62%), Space: 90.9 MB (26.84%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // If number already seen
+            if (map.containsKey(nums[i])) {
+                int prevIndex = map.get(nums[i]);
+
+                // Check distance
+                if (i - prevIndex <= k) {
+                    return true;
+                }
+            }
+
+            // Update latest index
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
