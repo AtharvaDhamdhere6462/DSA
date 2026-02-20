d164b3ac0374f04aa03c0b652572e08ab9a077e1
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-02-20 08:21:47 +0530

    Time: 17 ms (99.61%), Space: 81.5 MB (55%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -24,27 +24,46 @@
 //     }
 // }
 
+// import java.util.*;
+
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         Set<Integer> window = new HashSet<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+
+//             // If already present in window → duplicate within k
+//             if (window.contains(nums[i])) {
+//                 return true;
+//             }
+
+//             window.add(nums[i]);
+
+//             // Maintain window size at most k
+//             if (window.size() > k) {
+//                 window.remove(nums[i - k]);
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Set<Integer> window = new HashSet<>();
+        Set<Integer> freq = new HashSet<>(k);
+        for (int i = 0; i <nums.length ; i++) {
+                if(i>k){
+                    freq.remove(nums[i-k-1]);
+                }
 
-        for (int i = 0; i < nums.length; i++) {
-
-            // If already present in window → duplicate within k
-            if (window.contains(nums[i])) {
-                return true;
-            }
-
-            window.add(nums[i]);
-
-            // Maintain window size at most k
-            if (window.size() > k) {
-                window.remove(nums[i - k]);
-            }
-        }
-
-        return false;
+                if(!freq.add(nums[i])){
+                    return true;
+                }
+        
     }
+        return false;
+}
 }
