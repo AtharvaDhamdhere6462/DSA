01a8e9233b0b9f365eab6081123e4900f91c100a
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-03-03 13:45:34 +0530

    Time: 1424 ms (5.17%), Space: 49.6 MB (13.79%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,22 +1,49 @@
+// class Solution {
+//     public int[] replaceElements(int[] arr) {
+//         int n=arr.length;
+//         int[] temp= new int[n];
+
+//         for(int i=0;i<n;i++){
+//             if(i == n - 1){
+//                 temp[i] = -1;
+//             } 
+//             int max=0;
+//             for(int j=i+1;j<n;j++){
+//                 max=Math.max(max,arr[j]);
+//                 temp[i]=max;
+                
+//             }
+            
+//         }
+        
+        
+//         return temp;
+//     }
+// }
+
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int n=arr.length;
-        int[] temp= new int[n];
+        int n = arr.length;
+        int[] temp = new int[n];
 
-        for(int i=0;i<n;i++){
-            if(i == n - 1){
+        for(int i = 0; i < n; i++) {
+
+            if(i == n - 1) {
                 temp[i] = -1;
             } 
-            int max=0;
-            for(int j=i+1;j<n;j++){
-                max=Math.max(max,arr[j]);
-                temp[i]=max;
-                
+            else {
+                int max = arr[i + 1];   // start from first right element
+
+                for(int j = i + 1; j < n; j++) {
+                    if(arr[j] > max) {
+                        max = arr[j];
+                    }
+                }
+
+                temp[i] = max;  // assign after loop
             }
-            
         }
-        
-        
+
         return temp;
     }
 }
