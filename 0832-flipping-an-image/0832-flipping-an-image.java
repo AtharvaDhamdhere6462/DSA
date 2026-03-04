a3854c6a565f9dad692cc9f46c13663b145e2bb8
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-03-04 17:21:37 +0530

    Time: 0 ms (100%), Space: 45.6 MB (87.55%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -1,27 +1,47 @@
+// class Solution {
+//     public int[][] flipAndInvertImage(int[][] image) {
+
+//         int n = image.length;
+
+//         for(int i = 0; i < n; i++){
+
+//             // flip row
+//             for(int j = 0; j < n/2; j++){
+//                 int temp = image[i][j];
+//                 image[i][j] = image[i][n-1-j];
+//                 image[i][n-1-j] = temp;
+//             }
+
+//             // invert values
+//             for(int j = 0; j < n; j++){
+//                 if(image[i][j] == 0){
+//                     image[i][j] = 1;
+//                 } else {
+//                     image[i][j] = 0;
+//                 }
+//             }
+//         }
+
+//         return image;
+//     }
+// }
+
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
-
+        
         int n = image.length;
-
+        
         for(int i = 0; i < n; i++){
-
-            // flip row
-            for(int j = 0; j < n/2; j++){
-                int temp = image[i][j];
-                image[i][j] = image[i][n-1-j];
-                image[i][n-1-j] = temp;
-            }
-
-            // invert values
-            for(int j = 0; j < n; j++){
-                if(image[i][j] == 0){
-                    image[i][j] = 1;
-                } else {
-                    image[i][j] = 0;
-                }
+            
+            for(int j = 0; j < (n + 1) / 2; j++){
+                
+                int temp = image[i][j] ^ 1;
+                image[i][j] = image[i][n - 1 - j] ^ 1;
+                image[i][n - 1 - j] = temp;
+                
             }
         }
-
+        
         return image;
     }
 }
