3588d9e6e38fb9a7ba768a832d42fe7e18763146
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-03-04 16:32:15 +0530

    Time: 17 ms (81.54%), Space: 49.7 MB (74.57%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,17 +1,31 @@
-class Solution {
-    public int arrayPairSum(int[] nums) {
-        int n=nums.length;
-        Arrays.sort(nums);
-        int left=0;
+// class Solution {
+//     public int arrayPairSum(int[] nums) {
+//         int n=nums.length;
+//         Arrays.sort(nums);
+//         int left=0;
          
-        int count =0;
-        for(int right=left+1;right<n;right+=2){
-            int num=Math.min(nums[left],nums[right]);
-            count +=num;
-            left+=2;
-        }
+//         int count =0;
+//         for(int right=left+1;right<n;right+=2){
+//             int num=Math.min(nums[left],nums[right]);
+//             count +=num;
+//             left+=2;
+//         }
             
         
-        return count;
+//         return count;
+//     }
+// }
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int sum = 0;
+        
+        for(int i = 0; i < nums.length; i += 2){
+            sum += nums[i];
+        }
+        
+        return sum;
     }
 }
