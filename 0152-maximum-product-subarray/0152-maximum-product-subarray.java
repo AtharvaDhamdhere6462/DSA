3829381911f2f0c40858580b67c9da816aac434a
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-05-15 20:28:27 +0530

    Time: 2 ms (70.19%), Space: 47.2 MB (72.29%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -38,24 +38,49 @@
 // }
 
 
+// class Solution {
+//     public int maxProduct(int[] nums) {
+
+//         int n = nums.length;
+//         int maxProduct = Integer.MIN_VALUE;
+
+//         for (int i = 0; i < n; i++) {
+
+//             int product = 1;
+
+//             for (int j = i; j < n; j++) {
+
+//                 product = product * nums[j];
+
+//                 maxProduct = Math.max(maxProduct, product);
+//             }
+//         }
+
+//         return maxProduct;
+//     }
+// }
+
+
+
 class Solution {
     public int maxProduct(int[] nums) {
+        int n=nums.length;
+        int maxi= nums[0];
+        int mini = nums[0];
+        int maxpro= nums[0];
 
-        int n = nums.length;
-        int maxProduct = Integer.MIN_VALUE;
-
-        for (int i = 0; i < n; i++) {
-
-            int product = 1;
-
-            for (int j = i; j < n; j++) {
-
-                product = product * nums[j];
-
-                maxProduct = Math.max(maxProduct, product);
+        for(int i=1; i<n;i++){
+            if(nums[i]<0){
+                int temp =maxi;
+                maxi=mini;
+                mini=temp;
             }
+            maxi=Math.max(maxi*nums[i], nums[i]);
+            mini= Math.min(mini*nums[i],nums[i]);
+            maxpro= Math.max(maxpro,maxi);
+
         }
 
-        return maxProduct;
+        return maxpro;
     }
 }
