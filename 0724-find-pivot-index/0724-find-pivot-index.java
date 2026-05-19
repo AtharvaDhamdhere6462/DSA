2cb0678243ff31a3820036c1e1d0fd906f84424a
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-05-19 16:02:29 +0530

    Time: 1 ms (98.47%), Space: 47.8 MB (8.46%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,27 +1,56 @@
-class Solution {
-    public int pivotIndex(int[] nums) {
-        int n = nums.length;
-        int[] prefix = new int[n];
-        prefix[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] + nums[i];
-            }
+// class Solution {
+//     public int pivotIndex(int[] nums) {
+//         int n = nums.length;
+//         int[] prefix = new int[n];
+//         prefix[0] = nums[0];
+//         for(int i = 1; i < n; i++) {
+//             prefix[i] = prefix[i-1] + nums[i];
+//             }
 
         
-        int[] suffix = new int[n];
-        suffix[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] + nums[i];
-            }
+//         int[] suffix = new int[n];
+//         suffix[n - 1] = nums[n - 1];
+//         for (int i = n - 2; i >= 0; i--) {
+//             suffix[i] = suffix[i + 1] + nums[i];
+//             }
 
 
-        for(int i = 0; i < n; i++) {
-            int left = prefix[i] - nums[i];
-            int right = suffix[i] - nums[i];
+//         for(int i = 0; i < n; i++) {
+//             int left = prefix[i] - nums[i];
+//             int right = suffix[i] - nums[i];
 
-            if(left == right) {
+//             if(left == right) {
+//                 return i;
+//             }
+//         }return -1;
+//     }
+// }
+
+
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n=nums.length;
+        int [] prefix= new int[n];
+        prefix[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefix[i]= prefix[i-1]+nums[i];
+        }
+
+        int [] suffix= new int[n];
+        suffix[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            suffix[i]= suffix[i+1]+nums[i];
+        }
+
+        for(int i=0;i<n;i++){
+            int left= prefix[i]-nums[i];
+            int right= suffix[i]-nums[i];
+
+            if(left==right){
                 return i;
             }
-        }return -1;
+        }
+        return -1;
     }
 }
