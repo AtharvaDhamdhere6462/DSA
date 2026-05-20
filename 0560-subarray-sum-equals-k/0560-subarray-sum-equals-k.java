0ae7921e5de4b91b584763f0b7eefd51dfc42119
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-05-21 01:31:50 +0530

    Time: 20 ms (98.38%), Space: 49.3 MB (5.67%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -16,42 +16,71 @@
 // }
 
 
+// class Solution {
+//     public int subarraySum(int[] nums, int k) {
+//         int n = nums.length;
+//         int count = 0;
+
+//         // prefix sum array
+//         int[] prefixSum = new int[n];
+//         prefixSum[0] = nums[0];
+
+//         for (int i = 1; i < n; i++) {
+//             prefixSum[i] = prefixSum[i - 1] + nums[i];
+//         }
+
+//         // HashMap: prefixSum value -> frequency
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for (int j = 0; j < n; j++) {
+
+//             // case 1: subarray from 0 to j
+//             if (prefixSum[j] == k) {
+//                 count++;
+//             }
+
+//             int val = prefixSum[j] - k;
+
+//             // case 2: subarray ending at j
+//             if (map.containsKey(val)) {
+//                 count += map.get(val);
+//             }
+
+//             // update frequency of current prefix sum
+//             map.put(prefixSum[j], map.getOrDefault(prefixSum[j], 0) + 1);
+//         }
+
+//         return count;
+//     }
+// }
+
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int n = nums.length;
-        int count = 0;
+        int n=nums.length;
+        int count =0;
 
-        // prefix sum array
-        int[] prefixSum = new int[n];
-        prefixSum[0] = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        int[] prefix=new int[n];
+        prefix[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1] + nums[i];
         }
 
-        // HashMap: prefixSum value -> frequency
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map= new HashMap<>();
 
-        for (int j = 0; j < n; j++) {
-
-            // case 1: subarray from 0 to j
-            if (prefixSum[j] == k) {
+        for(int j=0;j<n;j++){
+            if(prefix[j]==k){
                 count++;
             }
+            int val = prefix[j]-k;
 
-            int val = prefixSum[j] - k;
-
-            // case 2: subarray ending at j
-            if (map.containsKey(val)) {
-                count += map.get(val);
+            if(map.containsKey(val)){
+                count +=map.get(val);
             }
 
-            // update frequency of current prefix sum
-            map.put(prefixSum[j], map.getOrDefault(prefixSum[j], 0) + 1);
+            map.put(prefix[j],map.getOrDefault(prefix[j],0)+1);
         }
+    
+    return count;
 
-        return count;
     }
 }
-
-
