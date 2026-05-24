0ce99948d0008bbcffb7b2251b3dae4afad187c0
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-05-24 16:39:13 +0530

    Time: 7 ms (1.13%), Space: 47.2 MB (54.43%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -11,20 +11,41 @@
 //     }
 // }
 
+// class Solution {
+//     public int[] getConcatenation(int[] nums) {
+//         int n = nums.length;
+//         int[] temp =new int[n*2];
+//         Map<Integer,Integer> map=new HashMap<>();
+//         for(int i=0;i<n;i++){
+//             map.put(i,nums[i]);
+//         }
+//         for(int i=0;i<n;i++){
+//             temp[i]=map.get(i);
+//             temp[i+n]=map.get(i);
+//         }
+        
+//         return temp;
+
+//     }
+// }
+
+
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int n = nums.length;
+        int n=nums.length;
         int[] temp =new int[n*2];
-        Map<Integer,Integer> map=new HashMap<>();
+
+        Map<Integer,Integer> map= new HashMap<>();
+
         for(int i=0;i<n;i++){
             map.put(i,nums[i]);
         }
+
         for(int i=0;i<n;i++){
             temp[i]=map.get(i);
             temp[i+n]=map.get(i);
         }
-        
-        return temp;
 
+        return temp;
     }
 }
