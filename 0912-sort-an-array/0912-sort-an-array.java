f0c3bd812b2a47b52b87be69bd726ec25ee70eaa
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-05-25 16:46:10 +0530

    Time: 183 ms (10.64%), Space: 102.1 MB (5.13%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,6 +1,33 @@
+// class Solution {
+//     public int[] sortArray(int[] nums) {
+//         Arrays.sort(nums);
+//         return nums;
+//     }
+// }
+
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        int n=nums.length;
+        int[] temp = new int[n];
+
+        Map<Integer,Integer> map= new TreeMap<>();
+
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        int index = 0;
+
+        // Put elements back in sorted order
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+
+            while (freq > 0) {
+                nums[index] = key;
+                index++;
+                freq--;
+            }
+        }
+
         return nums;
     }
 }
