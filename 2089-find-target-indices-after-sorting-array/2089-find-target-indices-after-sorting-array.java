c743a3a0d7cf2e6ee50952464624bf19f631a70c
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-06-10 11:34:14 +0530

    Time: 6 ms (25.16%), Space: 46.4 MB (43.09%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.java
@@ -1,12 +1,72 @@
+// class Solution {
+//     public List<Integer> targetIndices(int[] nums, int target) {
+//         Arrays.sort(nums);
+
+//         List<Integer> ans = new ArrayList<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] == target) {
+//                 ans.add(i);
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
         Arrays.sort(nums);
 
         List<Integer> ans = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                ans.add(i);
+        int first = firstOccurrence(nums, target);
+        if (first == -1) return ans;
+
+        int last = lastOccurrence(nums, target);
+
+        for (int i = first; i <= last; i++) {
+            ans.add(i);
+        }
+
+        return ans;
+    }
+
+    private int firstOccurrence(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1; // search left
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private int lastOccurrence(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1; // search right
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
