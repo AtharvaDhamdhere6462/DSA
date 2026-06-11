be04ec33465a12c7b2ba76c094e5bbb426e73e85
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-06-11 07:53:54 +0530

    Time: 6 ms (98.93%), Space: 51.4 MB (15.64%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,6 +1,45 @@
+// class Solution {
+//     public int smallestDivisor(int[] nums, int threshold) {
+
+//         int max = 0;
+
+//         // Find maximum element using simple loop
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] > max) {
+//                 max = nums[i];
+//             }
+//         }
+
+//         int low = 1;
+//         int high = max;
+//         int answer = max;
+
+//         while (low <= high) {
+
+//             int mid = low + (high - low) / 2;
+//             int sum = 0;
+
+//             // Calculate sum using simple loop
+//             for (int i = 0; i < nums.length; i++) {
+//                 sum += (nums[i] + mid - 1) / mid;  // ceil division
+//             }
+
+//             if (sum <= threshold) {
+//                 answer = mid;
+//                 high = mid - 1;   // try smaller divisor
+//             } else {
+//                 low = mid + 1;    // need bigger divisor
+//             }
+//         }
+
+//         return answer;
+//     }
+// }
+
+
 class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
-        int n= nums.length;
+        int n=  nums.length;
         int max=0;
         for(int i=0;i<n;i++){
             if(nums[i]>max){
@@ -9,25 +48,24 @@ class Solution {
         }
 
         int low=1;
-        int high=max;
+        int high= max;
         int ans=max;
 
         while(low<=high){
             int mid=low+(high-low)/2;
-
             int sum=0;
+
             for(int i=0;i<n;i++){
-                sum+=(nums[i]+mid-1)/mid;
+                sum += (nums[i] + mid - 1) / mid;
             }
 
-            if(sum<=threshold){
-                ans =mid;
+            if(sum<= threshold){
+                ans=mid;
                 high=mid-1;
             }else{
                 low=mid+1;
             }
         }
         return ans;
-       
     }
 }
