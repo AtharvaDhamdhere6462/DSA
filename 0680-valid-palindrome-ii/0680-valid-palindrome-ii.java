ddd1311e61328aa8fb02bcd1d7310f5b5d543ef6
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-06-16 09:14:15 +0530

    Time: 4 ms (99.7%), Space: 48 MB (11.92%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,26 +1,53 @@
+// class Solution {
+//     public boolean validPalindrome(String s) {
+//         int i = 0;
+//         int j = s.length() - 1;
+
+//         while (i < j) {
+//             if (s.charAt(i) != s.charAt(j)) {
+//                 return check(s, i + 1, j) || check(s, i, j - 1);
+//             }
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+
+//     public boolean check(String s, int i, int j) {
+//         while (i < j) {
+//             if (s.charAt(i) != s.charAt(j)) {
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean validPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return check(s, i + 1, j) || check(s, i, j - 1);
+        int n= s.length();
+        int low=0;
+        int high=n-1;
+        while(low<high){
+            if(s.charAt(low) != s.charAt(high)){
+                return check(s,low+1,high) || check(s,low,high-1);
             }
-            i++;
-            j--;
-        }
-        return true;
+            low++;
+            high--;
+        }return true;
+
+
     }
 
-    public boolean check(String s, int i, int j) {
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
+    public boolean check(String s,int low ,int high){
+        while(low<high){
+        if(s.charAt(low)!= s.charAt(high)){
+            return false;
         }
-        return true;
+        low++;
+        high--;
+    }return true;
     }
 }
