03538ad425e9aa0b53fcb7d8ab7e937705236f82
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-06-16 09:47:43 +0530

    Time: 1 ms (94.92%), Space: 42.9 MB (27.05%) - LeetHub

--- 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks/2379-minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,8 +1,37 @@
+// class Solution {
+//     public int minimumRecolors(String blocks, int k) {
+//         int n=blocks.length();
+//         int count=0;
+
+//         for(int i=0;i<k;i++){
+//             if(blocks.charAt(i)=='W'){
+//                 count++;
+//             }
+//         }
+//         int min= count;
+//         int left=0;
+//         for(int right=k;right<n;right++){
+//             if(blocks.charAt(left) == 'W'){
+//                 count--;
+//             }
+//             left++;
+
+//             // add right element
+//             if(blocks.charAt(right) == 'W'){
+//                 count++;
+//             }
+
+//             min = Math.min(min, count);
+//             }
+            
+//         return min;
+//     }
+// }
+
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        int n=blocks.length();
+        int n= blocks.length();
         int count=0;
-
         for(int i=0;i<k;i++){
             if(blocks.charAt(i)=='W'){
                 count++;
@@ -10,20 +39,19 @@ class Solution {
         }
         int min= count;
         int left=0;
-        for(int right=k;right<n;right++){
-            if(blocks.charAt(left) == 'W'){
+        for(int r=k;r<n;r++){
+            if(blocks.charAt(left)=='W'){
                 count--;
-            }
-            left++;
 
-            // add right element
-            if(blocks.charAt(right) == 'W'){
+            }left++;
+
+            if(blocks.charAt(r)=='W'){
                 count++;
             }
 
-            min = Math.min(min, count);
-            }
-            
+            min=Math.min(min,count);
+        }
+
         return min;
     }
 }
