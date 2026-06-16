3f3d8754de87eb5e2d66befe6b1c10f7da7b410c
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-06-16 10:14:41 +0530

    Time: 5 ms (36.2%), Space: 43.3 MB (84.04%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,28 +1,53 @@
+// class Solution {
+//     public String removeOuterParentheses(String s) {
+
+//         int count = 0;
+//         String result = "";
+
+//         for(int i = 0; i < s.length(); i++) {
+
+//             if(s.charAt(i) == '(') {
+
+//                 if(count > 0) {
+//                     result += s.charAt(i);
+//                 }
+//                 count++;
+//             }
+//             else {
+
+//                 count--;
+
+//                 if(count > 0) {
+//                     result += s.charAt(i);
+//                 }
+//             }
+//         }
+
+//         return result;
+//     }
+// }
+
 class Solution {
     public String removeOuterParentheses(String s) {
 
+        StringBuilder ans = new StringBuilder();
         int count = 0;
-        String result = "";
 
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
 
-            if(s.charAt(i) == '(') {
-
-                if(count > 0) {
-                    result += s.charAt(i);
+            if (s.charAt(i) == '(') {
+                if (count > 0) {
+                    ans.append('(');
                 }
                 count++;
-            }
-            else {
-
+            } else {
                 count--;
-
-                if(count > 0) {
-                    result += s.charAt(i);
+                if (count > 0) {
+                    ans.append(')');
                 }
             }
         }
 
-        return result;
+        return ans.toString();
     }
 }
