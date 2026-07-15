3271c1c373abeaeac59d61d5066bbd0d22433037
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-07-15 07:42:09 +0530

    Time: 1 ms (99.49%), Space: 47.1 MB (72%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.java
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -1,11 +1,24 @@
+// class Solution {
+//     public int[] buildArray(int[] nums) {
+//        int[] temp = new int[nums.length];
+//        int index=0;
+//        for(int i=0;i<nums.length;i++){
+//         index=nums[i];
+//         temp[i] =nums[index];
+//        }
+//        return temp;
+//     }
+// }
+
+
 class Solution {
     public int[] buildArray(int[] nums) {
-       int[] temp = new int[nums.length];
-       int index=0;
-       for(int i=0;i<nums.length;i++){
-        index=nums[i];
-        temp[i] =nums[index];
-       }
-       return temp;
+        int n=nums.length;
+        int[] ans = new int[n];
+        for(int i=0;i<n;i++){
+            ans[i]=nums[nums[i]];
+        }
+        return ans;
     }
+
 }
