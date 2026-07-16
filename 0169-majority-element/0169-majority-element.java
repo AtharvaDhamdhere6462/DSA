655c630bfbc938fea70c7cc83154f158126e5918
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-07-16 07:55:12 +0530

    Time: 12 ms (36.63%), Space: 52.5 MB (85.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,83 +1,57 @@
 // class Solution {
 //     public int majorityElement(int[] nums) {
-//         int n = nums.length;
-//         int count = 0;
-
-//         for (int i = 0; i < n; i++) {
-            
-//             for (int j = 0; j < n; j++) {
-//                 if (nums[i] == nums[j]) {
-//                     count++;
-//                 }
-//             }
-//             if (count > n / 2) {
-//                 return nums[i];  
-//             }
-//         }
-
-//         return -1;
-//     }
-// }
-
-// import java.util.*;
-
-// class Solution {
-//     public int majorityElement(int[] nums) {
-//         Arrays.sort(nums);
-
-//         int freq = 1;
-//         int majority = nums[0];
-
-//         for (int i = 1; i < nums.length; i++) {
-//             if (nums[i] == nums[i - 1]) {
-//                 freq++;
-//             } else {
-//                 freq = 1;
-//                 majority = nums[i];
-//             }
-
-//             if (freq > nums.length / 2) {
-//                 return nums[i];
-//             }
-//         }
-//         return majority;
-//     }
-// }
-
-
-
-// class Solution{
-//     public int majorityElement(int [] nums){
-//         int n= nums.length;
+//         int n=nums.length;
 //         int freq=0;
 //         int ans=0;
+
 //         for(int i=0;i<n;i++){
 //             if(freq==0){
-//                 ans= nums[i];
-//             }if(ans ==nums[i]){
+//                 ans=nums[i];
+               
+//             }if(nums[i]==ans){
 //                 freq++;
 //             }else{
 //                 freq--;
 //             }
-//         }return ans;
+//         }
+//         return ans;
 //     }
 // }
 
-class Solution{
-    public int majorityElement(int [] nums){
-        int n =nums.length;
-        HashMap<Integer ,Integer> map = new HashMap<>();
 
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int n=nums.length;
+//         HashMap<Integer,Integer> map = new HashMap<>();
+//         for(int i=0;i<n;i++){
+//             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+
+//         }
+//         Set<Integer> keyset = map.keySet();
+
+//         for(Integer key : keyset){
+//             if(map.get(key) > n/2){
+//                 return key;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<n;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
 
-
-        for(Integer key:map.keySet()){
-            if (map.get(key)>nums.length/2){
+        for(int key:map.keySet()){
+            if(map.get(key)>n/2){
                 return key;
             }
         }
         return -1;
+
     }
 }
