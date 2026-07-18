2cf5c4a2a21d084408fb820379274b99c00405d4
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-07-18 07:47:15 +0530

    Time: 0 ms (100%), Space: 43.2 MB (95.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -65,24 +65,52 @@
 // }
 // }
 
+// class Solution {
+//     public void sortColors(int nums[]) {
+//         int n = nums.length;
+
+//         Map<Integer, Integer> map = new TreeMap<>();
+
+//         for (int i = 0; i < n; i++) {
+//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//         }
+
+//         int index = 0;
+//         for (int key : map.keySet()) {
+//             int freq = map.get(key);
+//             while (freq > 0) {
+//                 nums[index] = key;
+//                 index++;
+//                 freq--;
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public void sortColors(int nums[]) {
-        int n = nums.length;
-
-        Map<Integer, Integer> map = new TreeMap<>();
-
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-
-        int index = 0;
-        for (int key : map.keySet()) {
-            int freq = map.get(key);
-            while (freq > 0) {
-                nums[index] = key;
-                index++;
-                freq--;
+        int n=nums.length;
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                swap(nums,mid,high);
+                
+                high--;
             }
         }
+        
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
