04203489ce60d042f9a102878ab85a38b51fc520
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-08-18 11:23:49 +0530

    Time: 11 ms (85.49%), Space: 46.2 MB (59.39%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -24,30 +24,49 @@
 //     }
 // }
 
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+
+//         int n = s.length();
+//         int[] lastSeen = {-1, -1, -1};  // for 'a', 'b', 'c'
+//         int count = 0;
+
+//         for (int i = 0; i < n; i++) {
+
+//             // update last seen index
+//             lastSeen[s.charAt(i) - 'a'] = i;
+
+//             // check if all characters appeared
+//             if (lastSeen[0] != -1 && 
+//                 lastSeen[1] != -1 && 
+//                 lastSeen[2] != -1) {
+
+//                 count += 1 + Math.min(
+//                             lastSeen[0],
+//                             Math.min(lastSeen[1], lastSeen[2])
+//                          );
+//             }
+//         }
+
+//         return count;
+//     }
+// }
 class Solution {
     public int numberOfSubstrings(String s) {
 
-        int n = s.length();
-        int[] lastSeen = {-1, -1, -1};  // for 'a', 'b', 'c'
-        int count = 0;
+        int n= s.length();
+        int[] lastseen={-1,-1,-1};
+        int count=0;
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
+            lastseen[s.charAt(i)-'a']=i;
 
-            // update last seen index
-            lastSeen[s.charAt(i) - 'a'] = i;
-
-            // check if all characters appeared
-            if (lastSeen[0] != -1 && 
-                lastSeen[1] != -1 && 
-                lastSeen[2] != -1) {
-
-                count += 1 + Math.min(
-                            lastSeen[0],
-                            Math.min(lastSeen[1], lastSeen[2])
-                         );
+            if(lastseen[0]!=-1 &&
+            lastseen[1]!=-1 &&
+            lastseen[2]!=-1){
+                count += 1 + Math.min(lastseen[0],Math.min(lastseen[1],lastseen[2]));
             }
         }
-
         return count;
     }
 }
