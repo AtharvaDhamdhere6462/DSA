a20fdb1ae243c5589bc127c6dccdc1f73d29b96f
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-08-21 11:53:53 +0530

    Time: 1 ms (98.74%), Space: 47.5 MB (89.88%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,28 +1,51 @@
+// class Solution {
+//     public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+//         int len = flowerbed.length;
+//         int count = 0;
+
+//         for (int i = 0; i < len; i++) {
+
+//             if (flowerbed[i] == 0) {
+
+//                 int prev = (i == 0) ? 0 : flowerbed[i - 1];
+//                 int next = (i == len - 1) ? 0 : flowerbed[i + 1];
+
+//                 if (prev == 0 && next == 0) {
+
+//                     flowerbed[i] = 1;
+//                     count++;
+//                 }
+//             }
+
+//             if (count >= n) {
+//                 return true;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+
+
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int n1 =flowerbed.length;
+        int count=0;
 
-        int len = flowerbed.length;
-        int count = 0;
+        for(int i=0;i<n1;i++){
 
-        for (int i = 0; i < len; i++) {
+            if(flowerbed[i]==0){
+                int prev= (i==0)? 0 : flowerbed[i-1];
+                int nex =(i==n1-1)? 0 :flowerbed[i+1];
 
-            if (flowerbed[i] == 0) {
-
-                int prev = (i == 0) ? 0 : flowerbed[i - 1];
-                int next = (i == len - 1) ? 0 : flowerbed[i + 1];
-
-                if (prev == 0 && next == 0) {
-
+                if(prev==0 && nex==0){
                     flowerbed[i] = 1;
                     count++;
                 }
             }
-
-            if (count >= n) {
-                return true;
-            }
+            
         }
-
-        return false;
+        return n<=count;
     }
 }
