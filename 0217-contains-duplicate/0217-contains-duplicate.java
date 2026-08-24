8c49ef8808bcc74d36fda51af67f458875b486cf
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-08-24 13:33:31 +0530

    Time: 18 ms (76.1%), Space: 108.1 MB (54.93%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,92 @@
-import java.util.HashSet;
+// // import java.util.HashSet;
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+//         int n=nums.length;
+//         for (int i=0;i<n;i++) {
+//             if (set.contains(nums[i])) {
+//                 return true;  // duplicate found
+//             }
+//             set.add(nums[i]);
+//         }
+//         return false;  // no duplicates
+//     }
+// }
+
+
+
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         int n=nums.length;
+//         Set<Integer> set= new HashSet<>();
+
+//         for(int i=0;i<n;i++){
+//             if(set.contains(nums[i])){
+//                 return true;
+//             }
+//             set.add(nums[i]);
+//         }
+    
+//     return false;
+
+//     }
+// }
+
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         int n=nums.length;
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for(int i=0;i<n;i++){
+//             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+//         }
+
+//         for(int key:map.keySet()){
+//             if(map.get(key)>=2){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (set.contains(num)) {
-                return true;  // duplicate found
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+            }else{
+                return true;
             }
-            set.add(num);
+            
         }
-        return false;  // no duplicates
+        return false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
