359fe8996fa094dbd824d6b0d2e2129c5cfec661
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-08-24 13:28:52 +0530

    Time: 2 ms (97.08%), Space: 44.9 MB (55.74%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -35,30 +35,74 @@
 // }
 
 
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         int n1 =nums1.length;
+//         int n2=nums2.length;
+//         HashSet<Integer> set1 =new HashSet<>();
+//         HashSet<Integer> resultset2=new HashSet<>();
+
+//         for(int num:nums1){
+//             set1.add(num);
+//         }
+//         for(int num:nums2){
+//             if(set1.contains(num)){
+//                 resultset2.add(num);
+//             }
+//         }
+
+
+//          int[] result = new int[resultset2.size()];
+//         int index = 0;
+        
+//         for(int num : resultset2) {
+//             result[index++] = num;
+//         }
+        
+//         return result;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         int n1 =nums1.length;
-        int n2=nums2.length;
-        HashSet<Integer> set1 =new HashSet<>();
-        HashSet<Integer> resultset2=new HashSet<>();
-
-        for(int num:nums1){
-            set1.add(num);
+        int n2= nums2.length;
+        int[] temp =new int[Math.max(n1,n2)];
+        int indx=0;
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i=0;i<n1;i++){
+            
+                set.add(nums1[i]);
         }
-        for(int num:nums2){
-            if(set1.contains(num)){
-                resultset2.add(num);
+
+        for(int i=0;i<n2;i++){
+            if(set.contains(nums2[i])){
+                temp[indx]=nums2[i];
+                indx++;
+                set.remove(nums2[i]);
             }
         }
-
-
-         int[] result = new int[resultset2.size()];
-        int index = 0;
         
-        for(int num : resultset2) {
-            result[index++] = num;
-        }
-        
-        return result;
+        int[] result = new int[indx];
+for (int i = 0; i < indx; i++) {
+    result[i] = temp[i];
+}
+return result;
     }
 }
+
+
+
+
+
+
