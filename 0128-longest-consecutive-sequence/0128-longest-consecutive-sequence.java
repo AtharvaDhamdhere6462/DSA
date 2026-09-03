09f3e6043cfaadb10388b2fef969d246558486af
AtharvaDhamdhere6462 <atharva.dhamdhere6462@gmail.com>
2026-09-03 08:38:29 +0530

    Time: 21 ms (95.93%), Space: 77.4 MB (90.84%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -33,35 +33,62 @@
 //     }
 // }
 
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+
+//         int n = nums.length;
+
+//         if (n == 0) return 0;
+
+//         Arrays.sort(nums);
+
+//         int count = 1;
+//         int max = 1;
+
+//         for (int i = 1; i < n; i++) {
+
+//             // skip duplicates
+//             if (nums[i] == nums[i - 1]) {
+//                 continue;
+//             }
+
+//             // consecutive
+//             if (nums[i] == nums[i - 1] + 1) {
+//                 count++;
+//             } else {
+//                 count = 1;
+//             }
+
+//             max = Math.max(max, count);
+//         }
+
+//         return max;
+//     }
+// }
+
 class Solution {
     public int longestConsecutive(int[] nums) {
-
-        int n = nums.length;
-
-        if (n == 0) return 0;
-
+        
         Arrays.sort(nums);
-
-        int count = 1;
-        int max = 1;
-
-        for (int i = 1; i < n; i++) {
-
-            // skip duplicates
+        int n =nums.length;
+        if(n==0)return 0;
+        int count=1;
+        int max=1;
+        for(int i=1;i<n;i++){
             if (nums[i] == nums[i - 1]) {
                 continue;
             }
-
-            // consecutive
-            if (nums[i] == nums[i - 1] + 1) {
+            if(nums[i]==nums[i-1]+1){
                 count++;
-            } else {
-                count = 1;
             }
+            
+            else{
+                count=1;
 
-            max = Math.max(max, count);
+            }
+            max=Math.max(count,max);
+            
         }
-
         return max;
     }
 }
